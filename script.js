f9b63dbe92028f70f8800e7103763231beaf8a0b
Shakoer <shakoeradhikarie@gmail.com>
2024-11-25 12:31:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const screen = document.getElementById("screen");
+
+function appendToScreen(input){
+  screen.value += input
+}
+
+function clearScreen(){
+  screen.value = "";
+}
+
+function calculate(){
+  try{
+    screen.value = eval(screen.value);
+  }
+  catch(error){
+    screen.value = "Error"
+  }
+}
